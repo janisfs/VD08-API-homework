bb613d3b9c138dfff7b6fdfe18a8ea1f73f44676
janisfs <janisfs@mail.ru>
2024-09-30 23:16:31 +0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -3,14 +3,20 @@ from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)
 
+
 @main.route('/')
 def quote():
-    response = requests.get('https://api.quotable.io/random')
-    if response.status_code == 200:
-        data = response.json()
-        quote = data.get('content')
-        author = data.get('author')
-    else:
-        quote = "Couldn't fetch quote at the moment. Please try again later."
+    try:
+        response = requests.get('https://api.quotable.io/random', verify=False)
+        if response.status_code == 200:
+            data = response.json()
+            quote = data.get('content')
+            author = data.get('author')
+        else:
+            quote = "Couldn't fetch quote at the moment. Please try again later."
+            author = ""
+    except requests.exceptions.RequestException as e:
+        quote = f"An error occurred: {e}"
         author = ""
+
     return render_template('quote.html', quote=quote, author=author)
